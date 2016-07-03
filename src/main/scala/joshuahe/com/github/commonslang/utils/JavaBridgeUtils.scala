79a8fe701c6c841e5cdb65be6fd302b67e8a39ff
jinghea <jing8h@gmail.com>
2016-07-03 19:40:17 +1000

    add new java bridge methods

--- src/main/scala/joshuahe/com/github/commonslang/utils/JavaBridgeUtils.scala
+++ src/main/scala/joshuahe/com/github/commonslang/utils/JavaBridgeUtils.scala
@@ -4,9 +4,15 @@ package joshuahe.com.github.commonslang.utils
 import java.lang.{String => JString, Integer => JInteger, Long => JLong}
 import java.util.{List => JList}
 
+import java.util.{Collection => JCollection, HashMap => JHashMap, List => JList, Map => JMap, Set => JSet}
+
+import scala.collection.Map
+import scala.collection.mutable.{HashMap => MHashMap, Map => MMap}
+
 /**
   * Created by jing on 21/06/16.
   */
+
 object JavaBridgeUtils {
 
   import scala.collection.JavaConversions._
@@ -19,6 +25,15 @@ object JavaBridgeUtils {
     items
   }
 
+  def map2JMap[A, B](items: Map[A, B]): JMap[A, B] = {
+
+    items.foldLeft(new JHashMap[A, B]())((last, e) => {
+
+      last.put(e._1, e._2)
+      last
+
+    })
+  }
 }
 
 
---
+++ src/test/scala/joshuahe/com/github/commonslang/utils/JavaBridgeUtilsTest.scala
@@ -0,0 +1,35 @@
+package joshuahe.com.github.commonslang.utils
+
+import java.text.SimpleDateFormat
+
+import joshuahe.com.github.commonslang.test.UnitSpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+  * Created by jing on 29/06/16.
+  */
+
+
+@RunWith(classOf[JUnitRunner])
+class JavaBridgeUtilsTest extends UnitSpec {
+
+  "get or else a None option" should "return the else object even it's null" in {
+
+    val empty: Option[String] = Option(null)
+
+    assertResult(null) {
+      empty.getOrElse(null)
+    }
+  }
+
+  "get or else a None option with function" should "return the function result" in {
+
+    val empty: Option[String] = Option(null)
+
+    assertResult(null) {
+      empty.getOrElse(null)
+    }
+  }
+
+}
